f7db56f6004414acad806e968e2470fcda908ae7
Hashemoto <h.alawneh@live.com>
2018-03-22 17:35:10 +0400

    Classes Identified 'workshop class 10 solution' atm.py

--- atm solution/atm.py
+++ atm solution/atm.py
@@ -9,34 +9,51 @@ def input_request ():
         return 0
     return input
 
-def widthraw_req(balance,request):
-    new_balance = balance -request
-    while request > 0:
-        if balance < request:
-            print "Out of Balance, only "+str(balance)+" is availble for widthraw!"
-            new_balance += request
+def prompt_request(select_atm):
+    while raw_input("if you Want to widthraw please enter 'Y' : ") =="Y":
+        requested_money = input_request()
+        balance = select_atm.widthraw_req(requested_money)
+        if balance ==0:
+            print "No more Widthraws can be done. \nYour current balance is Zero.\nThank you for using "+ select_atm.bank_name+" ATM"
             break
-        if request >=100:
-            print "Give 100"
-            request -= 100
-        elif request >= 50:
-            print "Give 50"
-            request -= 50
-        elif request >= 10:
-            print "Give 10"
-            request -=10
-        elif request >=5:
-            print "Give 5"
-            request -= 5
-        elif request >0:
-            print "Give "+str(request)
-            request =0
-    return new_balance
 
-balance =500
-while raw_input("if you Want to widthraw please enter 'Y' : ") =="Y":
-    request = input_request()
-    balance = widthraw_req(balance,request)
-    if balance ==0:
-        print "Your Balance Reached Zero."
-        break
+
+class ATM():
+    def __init__(self, acc_balance,bank_name):
+        self.balance = int(acc_balance)
+        self.bank_name = bank_name
+        print "Welcome to "+self.bank_name
+        print "Your Current Balance is : "+str(self.balance)
+        print "=================================="
+
+    def widthraw_req(self,request):
+        new_balance = self.balance - request
+        while request > 0:
+            if request > self.balance:
+                print "Out of Balance, only "+str(self.balance)+" is availble for widthraw!"
+                new_balance += request
+                break
+            if request >=100:
+                print "Give 100"
+                request -= 100
+            elif request >= 50:
+                print "Give 50"
+                request -= 50
+            elif request >= 10:
+                print "Give 10"
+                request -=10
+            elif request >=5:
+                print "Give 5"
+                request -= 5
+            elif request >0:
+                print "Give "+str(request)
+                request =0
+        self.balance = new_balance
+        print "Remaining balance is : "+str(self.balance)
+        print "=================================="
+        return self.balance
+
+balance1 =500
+atm1 = ATM(balance1,"ADCB Bank")
+prompt_request(atm1)
+#atm1.widthraw_req(19)
