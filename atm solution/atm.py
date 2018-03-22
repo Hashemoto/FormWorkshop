37e3fe926f3ca0008cd2088103dfd6d9d17ad72c
Hashemoto <h.alawneh@live.com>
2018-03-22 13:50:46 +0400

    updated widthraw to a function in atm.py

--- atm solution/atm.py
+++ atm solution/atm.py
@@ -3,31 +3,33 @@ def input_request ():
     try:
         input = int(raw_input(user_prompt))
     except:
-        print "Entered Value is not a Number!!!!!!!!!!"
-    print "you have entered this value "+str(input)
+        print "Requested Value is not a Number."
     if input <= 0:
-        print "Value should be more than Zero"
+        print "Requested Value should be more than Zero."
         return 0
     return input
 
-money_in_atm =500
-request =input_request()
-while request > 0:
-    if money_in_atm <request:
-        print "a maximum value of "+str(money_in_atm)+" can only be requested !"
-        break
-    if request >=100:
-        print "Give 100"
-        request -= 100
-    elif request >= 50:
-        print "Give 50"
-        request -= 50
-    elif request >= 10:
-        print "Give 10"
-        request -=10
-    elif request >=5:
-        print "Give 5"
-        request -= 5
-    elif request >0:
-        print "Give "+str(request)
-        request =0
+def widthraw_req(balance,request):
+    while request > 0:
+        if balance < request:
+            print "Out of Balance, only "+str(balance)+" is availble for widthraw!"
+            break
+        if request >=100:
+            print "Give 100"
+            request -= 100
+        elif request >= 50:
+            print "Give 50"
+            request -= 50
+        elif request >= 10:
+            print "Give 10"
+            request -=10
+        elif request >=5:
+            print "Give 5"
+            request -= 5
+        elif request >0:
+            print "Give "+str(request)
+            request =0
+
+balance =500
+request = input_request()
+widthraw_req(balance,request)
