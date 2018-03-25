2936b8a8a595c22d35aaa225e0c68cadf5f6100a
Hashemoto <h.alawneh@live.com>
2018-03-25 11:43:58 +0400

    Added withdraw history 'workshop class 12 solution' to atm.py

--- atm solution/atm.py
+++ atm solution/atm.py
@@ -1,3 +1,4 @@
+import time
 def input_request ():
     user_prompt = "Please enter the value to widthraw in numbers! : "
     try:
@@ -12,48 +13,48 @@ def input_request ():
 def prompt_request(select_atm):
     while raw_input("if you Want to widthraw please enter 'Y' : ") =="Y":
         requested_money = input_request()
-        balance = select_atm.widthraw_req(requested_money)
+        balance = select_atm.withdraw_req(requested_money)
         if balance ==0:
             print "No more Widthraws can be done. \nYour current balance is Zero.\nThank you for using "+ select_atm.bank_name+" ATM"
             break
-
+    if raw_input("Enter 'Y'es to see your Withdraw History : ")=="Y":
+        select_atm.p_receipt()
 
 class ATM():
     def __init__(self, acc_balance,bank_name):
         self.balance = int(acc_balance)
         self.bank_name = bank_name
+        self.withdraw_list = []
         print "Welcome to "+self.bank_name
         print "Your Current Balance is : "+str(self.balance)
         print "=================================="
 
-    def widthraw_req(self,request):
-        new_balance = self.balance - request
-        while request > 0:
-            if request > self.balance:
-                print "Out of Balance, only "+str(self.balance)+" is availble for widthraw!"
-                new_balance += request
-                break
-            if request >=100:
-                print "Give 100"
-                request -= 100
-            elif request >= 50:
-                print "Give 50"
-                request -= 50
-            elif request >= 10:
-                print "Give 10"
-                request -=10
-            elif request >=5:
-                print "Give 5"
-                request -= 5
-            elif request >0:
-                print "Give "+str(request)
-                request =0
-        self.balance = new_balance
-        print "Remaining balance is : "+str(self.balance)
+    def withdraw_req(self,request):
+        if request > self.balance:
+            print "Out of Balance, only "+str(self.balance)+" is availble for widthraw!"
+        else:
+            notes=[100,50,10,5,4,3,2,1]
+            self.balance -= request
+            for x in notes:
+                while x <= request:
+                    request -= x
+                    print "Give "+str(x)
+                    self.withdraw_list.append("Give "+str(x) +"  Was done on "+time.ctime())
+            print "Remaining balance is : "+str(self.balance)
         print "=================================="
         return self.balance
+
+    def p_receipt(self):
+        if len(self.withdraw_list) == 0:
+            print "\nYou did not do any withdraw request !"
+            print "=================================="
+        else:
+            print "\nYour withdraw history is :"
+            print "=================================="
+            for i in self.withdraw_list:
+                print i
 
 balance1 =500
 atm1 = ATM(balance1,"ADCB Bank")
 prompt_request(atm1)
-#atm1.widthraw_req(19)
+#atm1.withdraw_req(19)
